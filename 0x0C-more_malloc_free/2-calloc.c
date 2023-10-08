2b01fc667cd492e9e0775f77104a8d7d77076651
momotalaat <mgivermgiver@yahoo.com>
2023-10-08 00:47:38 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,9 +1,23 @@
 #include "main.h"
 
 /**
- * string_nconcat - a function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: length
- * Return: null if fais,s2 if n >= s2, '\0' if bassed
+ * _calloc - a function that allocates memory for an array, using malloc
+ * @nmemb: bytes
+ * @size: size
+ * Return: null if nmemb, size is 0 or malloc fails
+ *		returns a pointer to the allocated memory.
  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *call;
+	unsigned int x;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	call = malloc(nmemb * size);
+	if (call == NULL)
+		return (0);
+	for (x = 0; x < (nmemb * size); x++)
+		call[x] = 0;
+	return (call);
+}
