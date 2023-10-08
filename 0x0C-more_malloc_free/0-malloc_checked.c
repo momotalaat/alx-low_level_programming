a0f9b20866997e8e84e4c0fdc16539caa39189df
momotalaat <mgivermgiver@yahoo.com>
2023-10-07 23:48:55 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: amount of memory to allocate
+ * Return: 89 if memory failed to allocate
+ *		and the allocated memory if success
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *check;
+
+	check = malloc(b);
+	if (!check)
+		return (98);
+	return (check);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+
+#endif
