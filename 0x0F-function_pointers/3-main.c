5f88e318d2de48b1ab4fd8a13425a07e126ff7aa
momotalaat <mgivermgiver@yahoo.com>
2023-10-11 23:59:01 -0700

    all

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -5,7 +5,7 @@
  *main - Entry point
  *@argc: arguments counter
  *@argv: array of arguments
- *Return: result
+ *Return: operating result
  */
 
 int main(int argc, char *argv[])
@@ -26,7 +26,8 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((atoi(argv[3]) == 0 && *ptr == '/') || (atoi(argv[3]) == 0 && *ptr == '%'))
+	if ((atoi(argv[3]) == 0 && *ptr == '/')
+			|| (atoi(argv[3]) == 0 && *ptr == '%'))
 	{
 		printf("Error\n");
 		exit(100);
