2fe3c94f1488c51eceb87b48b00e22d4ac5426da
momotalaat <mgivermgiver@yahoo.com>
2023-10-20 09:27:30 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
-global main
-	extern printf
-
+section .text
+global    main
+extern    printf
 main:
-	mov edi, formater
-	xor eax, eax
-	call printf
-	mov eax, 0
-	ret
-formater: db 'Hello, Holberton',0
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
